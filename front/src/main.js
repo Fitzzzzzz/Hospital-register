9cc949c6e40b47e0572d70b84587517355030b63
Fitzz Wang <fitzz_wang@163.com>
2017-06-04 16:39:01 +0800

    init front

---
+++ front/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'vue-beauty/package/style/vue-beauty.min.css'
+import VueBeauty from 'vue-beauty'
+
+Vue.use(VueAxios, axios)
+Vue.use(VueBeauty)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
